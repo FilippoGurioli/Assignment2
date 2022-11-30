6f2f8a61cdd83dfe3fbfa2883621b50a99a8c2ec
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2022-11-30 03:05:04 +0100

    Added todo list in the first .ino comment

--- assignment-02/src/Arduino/smart_cm/smart_cm.ino
+++ assignment-02/src/Arduino/smart_cm/smart_cm.ino
@@ -11,6 +11,15 @@ WDT         900/600/300
 LT              600
 LCDT            900
 ST              900
+
+Cose da fare (in ordine di importanza secondo me):
+- riguardare tutti i define (gli include li ho già guardati) per i periodi e i WL, lt.TH, lt.T1, MINROT e MAXROT
+- fare video
+- riguardare tutti i periodi con quelli che ho suggerito sopra (biaginus)
+- gestire le librerie esterne di java (lib) e arduino (enableInterrupt e liquidCrystal)
+- gestire il servo tramite java
+- creare il protocollo di comunicazione della seriale (con tanto di passaggio del periodo, WL1, WL2, WL3)
+- vedere se c'è un modo per fare i cpp
 */
 #include "myLib/Tasks/Controller.h"
 #include "myLib/Scheduler.h"
